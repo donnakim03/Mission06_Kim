abb0800991e83c922fafa55d45811dc968b1ce2f
donnakim03 <dkim210@byu.edu>
2024-02-16 20:58:00 -0700

    Add project files.

---
+++ Mission06_Kim/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+// Donna Kim
+// Section 03
+// Description: This is my Web app for Joel Hilton, who is a film reviewer. It is connected to a database of his favorite films.
+
+using Microsoft.AspNetCore.Mvc;
+using Mission06_Kim.Models;
+using System.Diagnostics;
+
+namespace Mission06_Kim.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieContext _context;
+        public HomeController(MovieContext movieContext)
+        {
+            _context = movieContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetToKnowJoel() // Connect to GetToKnowJoel page
+        {
+            return View("GetToKnowJoel");
+        }
+
+        public IActionResult EnterMovies() // Connect to page with form to enter movies
+        {
+            return View("EnterMovies");
+        }
+
+        [HttpPost]
+        public IActionResult EnterMovies(Movie response)
+        {
+            _context.Movies.Add(response); // add record to the database
+            _context.SaveChanges();
+
+            return View("Confirmation", response);
+        }
+    }
+}
---
+++ Mission06_Kim/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission06_Kim.Models
+{
+    public class Movie // These will be the columns in the Movies table
+    {
+        [Key]
+        [Required]
+        public int MovieID { get; set; } // This is the key that is required
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Director { get; set; }
+        public string Rating { get; set; }
+        public bool? Edited { get; set; } // ? makes it not required
+        public string? LentTo { get; set; }
+        public string? Notes { get; set; }
+    }
+
+}
